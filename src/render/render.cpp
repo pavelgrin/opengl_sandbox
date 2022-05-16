b2d044d5de18839c0b4dc6ce69f170e4c72105b9
Pavel Grinevich <pavelgrinv@gmail.com>
2022-05-16 17:47:18 +0300

    Add glm lib as a submodule

--- src/render/render.cpp
+++ src/render/render.cpp
@@ -1,10 +1,19 @@
 #include "./render.hpp"
 
 #define STB_IMAGE_IMPLEMENTATION
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 #include <stb_image.h>
 
 void Render::init()
 {
+    glm::vec4 vec(1.0f, 0.0f, 0.0f, 1.0f);
+    glm::mat4 trans = glm::mat4(1.0f);
+    trans           = glm::translate(trans, glm::vec3(1.0f, 1.0f, 0.0f));
+    vec             = trans * vec;
+    std::cout << vec.x << vec.y << vec.z << std::endl;
+
     std::string vertex_path   = m_resources_dir + "shaders/shader.vs";
     std::string fragment_path = m_resources_dir + "shaders/shader.fs";
 
